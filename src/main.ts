719bdf9c53525e944a90a510623d89bd5029d3b2
code-lucky <2924249993@qq.com>
2023-08-26 00:16:14 +0800

    新增响应interceptor、新增请求interceptor

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { FormatResponseInterceptor } from './format-response.interceptor';
+import { InvokeRecordInterceptor } from './invoke-record.interceptor';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe())
+  app.useGlobalInterceptors(new FormatResponseInterceptor())
+  app.useGlobalInterceptors(new InvokeRecordInterceptor())
+
   const configService = app.get(ConfigService)
   await app.listen(configService.get('nest_server_port'));
 }
